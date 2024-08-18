39ce2f35cffe692a39e9f4aefbb38e76ee015af7
Rafael Martins Galdino <rafael_martins_182@hotmail.com>
2024-08-18 09:06:05 -0300

    Config folders and initial files

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { Header } from "./Components/Header";
+import { Sidenav } from "./Components/Sidnav";
+import { Resumo } from "./Pages/Resumo";
 import "./Style.css";
 
 function App() {
   return (
     <div>
+      <Sidenav />
+      <main>
+        <Header />
+        <Resumo />
+      </main>
     </div>
   )
 }
---
+++ src/Pages/Resumo.tsx
@@ -0,0 +1,5 @@
+export const Resumo = () => {
+    return (
+        <div></div>
+    )
+}
